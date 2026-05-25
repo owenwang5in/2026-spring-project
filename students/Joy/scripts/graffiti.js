902f1e62bcb82d48efa8e80933ba7ae1360da73f
Joy Y <joyyang.5in@gmail.com>
2026-05-25 11:27:36 +0800

    drew 67

--- students/Joy/scripts/graffiti.js
+++ students/Joy/scripts/graffiti.js
@@ -1,5 +1,45 @@
+/*
+*HTML elements
+*/
 const graffitiCanvas = document.getElementById('my-graffiti');
-
-console.log(graffitiCanvas);
-
 const surface = graffitiCanvas.getContext("2d");
+const cleanButton = document.getElementById("clean");
+
+/*
+*graggiti style.
+*/
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
+
+/*
+shapes
+*/
+function shapes(){
+surface.rect(150, 200, 100, 100);
+surface.stroke();
+
+surface.beginPath();
+surface.moveTo(200, 50);
+surface.lineTo(150, 200);
+//Add a 7.
+surface.moveTo(250, 50);
+surface.lineTo(350, 50);
+surface.moveTo(350, 50);
+surface.lineTo(270, 300);
+
+surface.closePath();
+surface.stroke();
+
+
+
+}
+shapes();
+
+/*
+*clean up
+*/
+function cleanCanvas(){
+    surface.clearRect(0, 0, 400, 400);
+}
+cleanButton.addEventListener("click", cleanCanvas);
