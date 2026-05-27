6915f77c3a20984d14fda3db511cd8c0ced5833a
Charlize C <charlizechang.5in@gmail.com>
2026-05-27 08:55:17 +0800

    philosophy

--- students/Charlize/scripts/graffiti.js
+++ students/Charlize/scripts/graffiti.js
@@ -1,6 +1,43 @@
-/*console.log('It works!');*/
+/*Html elements*/
+
+
 
 const graffitiCanvas = document.getElementById('my-graffiti');
 
 const surface = graffitiCanvas.getContext("2d")
+
+const cleanButton = document.getElementById("clean")
+
 console.log(graffitiCanvas);
+
+/*sty;e*/
+surface.lineWidth = 13;
+surface.lineJoin = "round";
+surface.strokeStyle = "red";
+surface.beginPath();
+surface.moveTo(200, 50);
+surface.lineTo(150, 200);
+surface.closePath();
+surface.stroke();
+
+surface.moveTo(250, 50);
+surface.lineTo(350, 50);
+surface.moveTo(350, 50);
+surface.lineTo(270, 300);
+
+
+/*shapes*/
+
+function shapes (){
+    surface.rect(150, 200, 100 ,100);
+    surface.stroke();
+
+}
+shapes();
+
+/*Clean*/
+function Clean(){
+    console.log('Clean');
+surface.clearRect(0, 0, 400, 400);
+}
+cleanButton.addEventListener("click", Clean);
