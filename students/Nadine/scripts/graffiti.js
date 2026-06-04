1749c9543f5a153d0be666d41d03f351ef6d2514
Nadine <nadinehsu.5in@gmail.com>
2026-06-04 15:53:23 +0800

    working on eraser

--- students/Nadine/scripts/graffiti.js
+++ students/Nadine/scripts/graffiti.js
@@ -3,9 +3,10 @@
  */
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
-const cleanButton = document.getElementById("clean");
 const colorInput = document.getElementById("color-input");
 const sizeInput = document.getElementById("size-input");
+const toolSelect = document.getElementById("tool-select");
+console.log(toolSelect);
 
 /*
  *Graffiti style.
@@ -24,33 +25,6 @@ function changeSize() {
 changeSize();
 sizeInput.addEventListener("change", changeSize);
 
-/*
- *Shapes
- */
-function shapes() {
-    surface.rect(150, 200, 100, 100);
-    surface.stroke();
-
-    surface.beginPath();
-    surface.moveTo(200, 50);
-    surface.lineTo(150, 200);
-    // Add a 7.
-    surface.moveTo(250, 50);
-    surface.lineTo(350, 50);
-    surface.moveTo(350, 50);
-    surface.lineTo(310, 300);
-    surface.closePath();
-    surface.stroke();
-}
-shapes();
-
-/*
- * Clean up.
- */
-function cleanCanvas() {
-    surface.clearRect(0, 0, 400, 400);
-}
-cleanButton.addEventListener("click", cleanCanvas);
 
 /*
  * Graffiti
