eeef4e87073766e6faa826e3cc0021897da2acf7
jasper <jasperliu.5in@gmail.com>
2026-06-15 13:31:59 +0800

    circle eraser

--- students/Jasper/scripts/graffiti.js
+++ students/Jasper/scripts/graffiti.js
@@ -1,12 +1,14 @@
+import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
+
 /*
  * HTML elements
  */
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
+const eraser = new CanvasEraser(surface);
 const colorInput = document.getElementById("color-input");
 const sizeInput = document.getElementById("size-input");
 const toolSelect = document.getElementById("tool-select");
-console.log(toolSelect);
 
 /*
  * Graffiti style.
@@ -17,19 +19,16 @@ surface.lineJoin = "round";
 changeColor()
 function changeColor() {
   surface.strokeStyle = colorInput.value;
-  console.log(colorInput.value);
 }
 
 changeSize()
 function changeSize() {
   surface.lineWidth = sizeInput.value;
-  console.log(sizeInput.value);
 }
 
 let tool;
 function changeTool() {
   tool = toolSelect.value;
-  console.log(tool);
 }
 changeTool();
 colorInput.addEventListener("change", changeColor);
@@ -63,17 +62,16 @@ shapes();
 function cleanCanvas() {
   surface.clearRect(0, 0, 400, 400);
 }
-oldX = 0
-oldY = 0
+let oldX = 0;
+let oldY = 0;
 function graffiti(event) {
   const x = event.offsetX;
   const y = event.offsetY;
-  console.log(x, y, event.buttons)
 
   if (event.buttons === 1) {
     if (tool === "eraser") {
-      width = sizeInput.value;
-      surface.clearRect(x - (width / 2), y - (width / 2), width, width);
+      const radius = sizeInput.value/2;
+      eraser.circle(x, y, radius)
     } else {
       surface.beginPath();
       surface.moveTo(oldX, oldY);
