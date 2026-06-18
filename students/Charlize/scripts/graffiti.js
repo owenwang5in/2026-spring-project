b8d19a217be6101b947beea174cb696f83ffad8e
Charlize C <charlizechang.5in@gmail.com>
2026-06-18 15:57:06 +0800

    mustache

--- students/Charlize/scripts/graffiti.js
+++ students/Charlize/scripts/graffiti.js
@@ -1,5 +1,8 @@
 import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
+import { StickyClickyImage } from "../../../scripts/sticky-clicky-image.js";
 
+const stickyMustache = new StickyClickyImage('mustache')
+stickyMustache.setContainerID('dog-picture')
 const graffitiCanvas = document.getElementById('my-graffiti');
 const surface = graffitiCanvas.getContext("2d");
 const eraser = new CanvasEraser(surface)
