01829a0873e370df2c6789a6043acb60c6b4012f
Adrian <suadrian.5in@gmail.com>
2026-06-18 15:18:13 +0800

    add poster

--- students/Adrian/scripts/graffiti.js
+++ students/Adrian/scripts/graffiti.js
@@ -1,5 +1,8 @@
+import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
+
 /* 
 *HTML elements
+
 */
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
@@ -7,12 +10,12 @@ const cleanButton = document.getElementById("clean");
 const colorinput = document.getElementById("color-input");
 const sizeinput = document.getElementById("size-input");
 const toolSelect = document.getElementById("tool-select");
+const eraser = new CanvasEraser(surface);
 /*
 *Graffiti style
 */
 
 surface.lineJoin = "round";
-console.log(colorinput);
 function changecolor() {
     surface.strokeStyle = colorinput.value;
 }
@@ -55,12 +58,13 @@ function graffiti(event) {
 
     const x = event.offsetX;
     const y = event.offsetY;
-    console.log(x, y, event.buttons);
+    
 
     if (event.buttons === 1) {
         if (tool === "eraser") {
-            const width = sizeinput.value;
-            surface.clearRect(x, y, width, width);
+           const radius = sizeinput.value/2;
+           eraser.circle(x,y,radius);
+
         } else {
             surface.beginPath();
             surface.moveTo(oldX, oldY);
