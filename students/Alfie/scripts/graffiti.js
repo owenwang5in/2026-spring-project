2e2f0750883e56a05e7cf510f58dad0ae8fce636
AlfieHO <alfieho.5in@gmail.com>
2026-06-18 15:18:19 +0800

    Add poster

--- students/Alfie/scripts/graffiti.js
+++ students/Alfie/scripts/graffiti.js
@@ -1,8 +1,12 @@
+import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
+
 /*
 * HTML elements
 */
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
+const eraser = new CanvasEraser(surface);
+
 const colorInput = document.getElementById("color-input");
 const sizeInput = document.getElementById("size-input");
 const toolSelect = document.getElementById("tool-select");
@@ -63,8 +67,9 @@ function graffiti(event) {
 
     if (event.buttons > 0) {
         if (tool === "eraser") {
-            const width = sizeInput.value;
-            surface.clearRect(x - (width / 2), y - (width / 2), width, width);
+            const radius = sizeInput.value / 2;
+            eraser.circle(x, y, radius);
+
         } else {
             surface.beginPath();
             surface.moveTo(oldX, oldY);
