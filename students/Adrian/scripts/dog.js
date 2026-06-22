6b6fe6481a76d4fba81fd713dddc6fa105e017e4
Adrian <suadrian.5in@gmail.com>
2026-06-22 17:46:57 +0800

    idk

---
+++ students/Adrian/scripts/dog.js
@@ -0,0 +1,16 @@
+import { ModelRotator } from "../../../scripts/model-rotator.js";
+
+const dog = new ModelRotator('dog-3D');
+
+// dog.loadCube();
+dog.animate();
+const modle = "/images/Duck.glb";
+const scale=1;
+const position={
+    x:0,
+    y:1,
+    z:5,
+
+};
+dog.load(modle, scale);
+dog.cameraPosition(position);
