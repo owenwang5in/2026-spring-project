9026e414a6e303676670ca48356022b16a4e3405
Will Chu <willchu.5in@gmail.com>
2026-06-22 11:31:55 +0800

    add duck

--- students/will/scripts/graffiti.js
+++ students/will/scripts/graffiti.js
@@ -1,4 +1,7 @@
 import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
+import { StickyClickyImage } from "../../../scripts/sticky-clicky-image.js";
+const stickyMustache = new StickyClickyImage('mustache');
+stickyMustache.setContainerID('dog-picture');
 /*
  * HTML elements
  */
