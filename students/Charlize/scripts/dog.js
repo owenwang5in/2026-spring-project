4d15bcfd0070889eebea3a5df5b851b0880026f6
Charlize C <charlizechang.5in@gmail.com>
2026-06-23 09:13:16 +0800

    other stuff

--- students/Charlize/scripts/dog.js
+++ students/Charlize/scripts/dog.js
@@ -1,17 +1,17 @@
 import { ModelRotator } from "../../../scripts/model-rotator.js";
 
 const dog = new ModelRotator('dog-3d');
-const model = "/images/Duck.glb";
+const model = "./images/Dobby.glb";
 const scale = 1;
 const position ={
-    x: 5,
-    y: 4,
-    z: -5,
+    x: 10,
+    y: 8,
+    z: -10,
 };
 
 
 /*dog.loadCube();*/
 dog.load(model, scale);
-dog.loadFloor();
+
 dog.cameraPosition(position);
 dog.animate();
