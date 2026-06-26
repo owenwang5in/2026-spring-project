3d9155bb83330b481ef178b2d7daf8fb0906463e
Nadine <nadinehsu.5in@gmail.com>
2026-06-26 21:01:09 +0800

    half completed webpage

--- students/Nadine/scripts/dog.js
+++ students/Nadine/scripts/dog.js
@@ -1,13 +1,15 @@
 import { ModelRotator } from "../../../scripts/model-rotator.js";
 
 const dog = new ModelRotator('dog-3d');
-const model = "/images/Duck.glb";
-const scale = 2;
+const model = "./images/Nadine_dog.glb";
+const scale = 1;
 const position ={
     x:3,
     y:3,
-    z:-5,
+    z:9,
 };
+
+
 dog.load(model, scale);
 dog.cameraPosition(position);
 
