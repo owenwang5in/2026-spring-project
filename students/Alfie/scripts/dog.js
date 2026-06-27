0a52b56862ba81917a5da213e02a6b0a89302842
AlfieHO <alfieho.5in@gmail.com>
2026-06-27 12:33:19 +0800

    Final

--- students/Alfie/scripts/dog.js
+++ students/Alfie/scripts/dog.js
@@ -1,19 +1,20 @@
 import { ModelRotator } from "../../../scripts/model-rotator.js";
 
 const dog = new ModelRotator('dog-3d');
-const model = "/images/Duck.glb";
-const scale = 2;
+const model = "./images/Alfie-dog2.glb";
+const scale = 1;
 const position = {
-    x: 4,
-    y: 3,
-    z: -5,
+    x: -10,
+    y: 6,
+    z: 7,
 }
 
 //dog.loadCube();
 
 dog.load(model, scale);
-dog.loadFloor();
+//dog.loadFloor();
 dog.cameraPosition(position);
 
-
 dog.animate();
+
+
