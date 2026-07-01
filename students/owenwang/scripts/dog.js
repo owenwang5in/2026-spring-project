93aae5b31b7f75dd4289b86a664c0fe3fba9ec15
owenwang5in <owenwang.5in@gmail.com>
2026-07-01 15:45:30 +0800

    finish some webpages

--- students/owenwang/scripts/dog.js
+++ students/owenwang/scripts/dog.js
@@ -1,15 +1,16 @@
 import { ModelRotator } from "../../../scripts/model-rotator.js";
+import { Color } from "three";
 
 const dog = new ModelRotator("dog-3d");
-const model = "/images/Duck.glb";
-const scale = 2;
+const model = "/images/Owen Dog.glb";
+const scale = 1;
 const position = {
-    x: 4,
-    y: 5,
-    z: -5,
+    x: 8,
+    y: 0,
+    z: -4,
 }
 
 dog.load(model, scale);
-dog.loadFloor();
 dog.cameraPosition(position);
+dog.scene.background = new Color(0xffffff);
 dog.animate();
--- students/owenwang/scripts/graffiti.js
+++ students/owenwang/scripts/graffiti.js
@@ -5,6 +5,7 @@ import { CanvasEraser } from "../../../scripts/canvas-eraser.js";
  */
 const graffitiCanvas = document.getElementById("my-graffiti");
 const surface = graffitiCanvas.getContext("2d");
+const eraser = new CanvasEraser(surface);
 const colorInput = document.getElementById("color-input");
 const sizeInput = document.getElementById("size-input");
 const toolSelect = document.getElementById("tool-select");
@@ -45,8 +46,8 @@ function graffiti(event) {
 
     if (event.buttons > 0) {
         if (tool === "eraser") {
-            const width = sizeInput.value;
-            surface.clearRect(x - width / 2, y - width / 2, width, width);
+            const radius = sizeInput.value / 2;
+            eraser.circle(x, y, radius);
         } else {
             surface.beginPath();
             surface.moveTo(oldX, oldY);
